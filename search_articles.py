221d813a96c1fa0a540fa28f336f5cf72c8fe9d5
Griffin Minster <griffinminster@gmail.com>
2023-12-04 13:06:55 -0600

    Initial internal search

---
+++ search_articles.py
@@ -0,0 +1,66 @@
+import requests
+
+
+def search_articles(baseurl):
+    options = ["headline", "author", "date", "keyword"]
+    found_filter = False
+    search_filter = ""
+
+    print("Enter the user_id of the user you would like to search through >")
+    user_id = input()
+    if not user_id.isnumeric():
+        print("Invalid user id. Exiting...")
+        return
+
+    for _ in range(5):
+        print(
+            "What do you want to search by? [Options: headline, author, date, keyword] >"
+        )
+        search_filter = input()
+        search_filter = search_filter.lower()
+
+        if search_filter in options:
+            found_filter = True
+            break
+        else:
+            print("Invalid search term. Try again.")
+    if not found_filter:
+        print("Too many attempts. Exiting...")
+        return
+
+    if search_filter == "date":
+        search_filter = "pubdate"
+
+    print("Enter your search term >")
+    search_term = input()
+    search_url = (
+        baseurl + f"/search?userid={user_id}&filter={search_filter}&term={search_term}"
+    )
+
+    res = requests.get(search_url)
+    if res.status_code != 200:
+        # failed:
+        print("Failed with status code:", res.status_code)
+        if res.status_code == 400:
+            # we'll have an error message
+            body = res.json()
+            print("Error message:", body)
+        #
+        return
+
+    body = res.json()
+    print("Here are your saved articles:\n")
+    for key, art_data in body["data"].items():
+        print(key)
+        print("  " + art_data["headline"])
+        print("  " + art_data["author"])
+        print("  " + art_data["pubdate"])
+        print()
+
+    print("If you want to download an article, enter the article ID >")
+    wanted_article_id = input()
+    if wanted_article_id not in body["data"].keys():
+        print("Invalid article id. Exiting...")
+        return
+        print("Invalid article id. Exiting...")
+        return
