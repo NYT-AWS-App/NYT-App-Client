3b60f36b1fc12686cc892576193c912262ca25d8
Griffin Minster <griffinminster@gmail.com>
2023-12-04 21:28:23 -0600

    Initial get users

---
+++ get_users.py
@@ -0,0 +1,30 @@
+import logging
+
+import requests
+
+
+def get_users(baseurl):
+    try:
+        url = baseurl + "/users"
+        res = requests.get(url)
+
+        if res.status_code != 200:
+            # failed:
+            print("Failed with status code:", res.status_code)
+            if res.status_code == 400:
+                # we'll have an error message
+                body = res.json()
+                print("Error message:", body["message"])
+            return
+        else:
+            body = res.json()
+            for user in body["users"]:
+                print(user["userid"])
+                print(f"  {user['username']}")
+                print(f"  {user['pwdhash']}")
+                print(f"  {user['bucketfolder']}")
+    except Exception as e:
+        logging.error("delete_article() failed:")
+        logging.error(f"url: {url}")
+        logging.error(e)
+        return
