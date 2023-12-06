d3230fc6e730f39e56c811a7878bf240b4518e59
Griffin Minster <griffinminster@gmail.com>
2023-12-05 22:03:33 -0600

    Take user id

--- download_article.py
+++ download_article.py
@@ -15,9 +15,11 @@ def download_article(baseurl):
     -------
     nothing
     """
-    print("Enter an articleid:")
+    print("Enter a user id:")
+    userid = input()
+    print("Enter an article id:")
     articleid = input()
-    url = baseurl + "/download/" + articleid
+    url = baseurl + "/download/" + articleid + "/" + userid
     res = requests.get(url)
     if res.status_code != 200:
         # failed:
@@ -35,5 +37,4 @@ def download_article(baseurl):
         filename = articleid + ".txt"
         with open(filename, "wb") as f:
             f.write(bytes)
-        print("Downloaded to", filename)
         print("Downloaded to", filename)
